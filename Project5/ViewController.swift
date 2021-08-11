f4cede1ed549eb68b1610e82f93423dc5e22e13b
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-08-11 12:57:47 +0300

    Extract words from start.txt

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -7,12 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startWords = try? String(contentsOf: startWordsUrl){
+                //try?:"call this code, and if it throws an error just send back nil"
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        
+        
     }
+    
+    
 
 
 }
